b6c78e3c90bafebe84101983cb4883630f18d4e5
RaJ <raj@Rajs-MacBook-Pro.local>
2019-12-06 00:13:23 -0600

    added taking turns

--- project4/src/index.js
+++ project4/src/index.js
@@ -14,13 +14,17 @@ function Square(props) {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
   
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
   
     renderSquare(i) {
@@ -33,7 +37,7 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
@@ -80,4 +84,6 @@ function Square(props) {
     <Game />,
     document.getElementById('root')
   );
+  
+  
   
